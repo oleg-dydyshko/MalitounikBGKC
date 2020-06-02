c7afa4c19b43e034977166efdde3139dd93b2740
Олег <oleg.dydyshko@gmail.com>
2020-06-02 22:47:50 +0300

    Выпраўленне памылак.

--- malitounik-bgkc/build/generated/source/buildConfig/debug/by/carkva_gazeta/malitounik/BuildConfig.java
+++ malitounik-bgkc/build/generated/source/buildConfig/debug/by/carkva_gazeta/malitounik/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "by.carkva_gazeta.malitounik";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 43621;
   public static final String VERSION_NAME = "3.7.10";
 }

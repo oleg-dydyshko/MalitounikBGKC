4da6963fb5da211ae7d90127cfae30b2b0c6f2bc
Олег <oleg.dydyshko@gmail.com>
2021-05-18 11:24:25 +0300

    Прынагодныя малітвы: - Малітва да сьв. Язэпа за мужчынаў - Малітва мужа і бацькі да сьв. Язэпа

--- malitounik-bgkc/src/main/kotlin/by/carkva_gazeta/malitounik/MenuListData.kt
+++ malitounik-bgkc/src/main/kotlin/by/carkva_gazeta/malitounik/MenuListData.kt
@@ -1,6 +1,6 @@
 package by.carkva_gazeta.malitounik
 
-data class MenuListData(val id: Int, val data: String, val type: String) : Comparable<MenuListData> {
+data class MenuListData(val data: String, val type: String) : Comparable<MenuListData> {
     override fun compareTo(other: MenuListData): Int {
         return this.data.compareTo(other.data, true)
     }

3f229d84c8b8a03513e0b7337438aaa23d057ef8
Олег <oleg.dydyshko@gmail.com>
2023-12-17 21:07:06 +0300

    Выпраўленне Бібліі

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 19
         targetSdk = 34
-        versionCode = 442176
-        versionName = "4.3.9.17"
+        versionCode = 442177
+        versionName = "4.3.9.18"
 
         //multiDexEnabled true
 

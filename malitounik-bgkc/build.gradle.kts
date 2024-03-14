b64ee9fed97c34a67b6fff6913a55779acc01be9
Олег <oleg.dydyshko@gmail.com>
2024-03-14 08:46:28 +0300

    Паляпшэнне пошуку па тэксту.

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 19
         targetSdk = 34
-        versionCode = 442229
-        versionName = "4.4.2.6"
+        versionCode = 442230
+        versionName = "4.4.2.7"
 
         //multiDexEnabled true
 

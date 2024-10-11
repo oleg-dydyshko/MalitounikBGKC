c0511315f839cf7f0f47e639472c5007c26f6dda
Олег <oleg.dydyshko@gmail.com>
2024-10-11 21:43:49 +0300

    Выпраўленне памылак.

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 442350
-        versionName = "5.0.2.14"
+        versionCode = 442351
+        versionName = "5.0.3"
 
         //multiDexEnabled = true
 

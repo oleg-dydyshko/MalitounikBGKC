d43af591358a6d56424ecda59a4bef0177937aa9
Олег <oleg.dydyshko@gmail.com>
2024-10-19 19:30:53 +0300

    Марыя, ці Ты ведала? (Калядныя)

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 442357
-        versionName = "5.0.3.6"
+        versionCode = 442358
+        versionName = "5.0.3.7"
 
         //multiDexEnabled = true
 

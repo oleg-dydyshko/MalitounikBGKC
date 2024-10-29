0b793687937f329c6b910eb672a43a810bcca897
oleg <oleg.dydyshko@gmail.com>
2024-10-29 16:52:56 +0300

    Выпраўленне Дзялення Святымі.

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 442361
-        versionName = "5.0.3.10"
+        versionCode = 442362
+        versionName = "5.0.3.11"
 
         //multiDexEnabled = true
 

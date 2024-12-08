aa8de04632c084222a73fb34c95316b492562141
oleg <oleg.dydyshko@gmail.com>
2024-12-08 08:19:42 +0300

    Рэліз 5.1.1

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 442387
-        versionName = "5.1.0.18"
+        versionCode = 442388
+        versionName = "5.1.1"
 
         //multiDexEnabled = true
 

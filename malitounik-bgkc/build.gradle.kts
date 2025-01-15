42641e268df2920264feb7a80ea233677c55d2da
oleg <oleg.dydyshko@gmail.com>
2025-01-15 21:43:02 +0300

    Малітвы за памерлых («Божа духаў і ўсякага цялеснага стварэньня»).

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 442419
-        versionName = "5.2.1.7"
+        versionCode = 442420
+        versionName = "5.2.1.8"
 
         //multiDexEnabled = true
 

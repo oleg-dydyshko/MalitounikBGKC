3ec8e5ac89c14ea27403d2e695e1ead03b979480
oleg <oleg.dydyshko@gmail.com>
2025-02-06 06:31:30 +0300

    Выдаленне некаторых парафій

--- malitounik-bgkc/build.gradle.kts
+++ malitounik-bgkc/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId =  "by.carkva_gazeta.malitounik"
         minSdk = 21
         targetSdk = 35
-        versionCode = 442425
-        versionName = "5.2.2.3"
+        versionCode = 442426
+        versionName = "5.2.3"
 
         //multiDexEnabled = true
 
